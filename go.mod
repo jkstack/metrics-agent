02c1e2877d7a6f8dcb99890ce062de3a94d1d1ed
lwch <lwch748@gmail.com>
2022-08-25 14:41:49 +0800

    实现静态数据获取逻辑

--- go.mod
+++ go.mod
@@ -3,32 +3,40 @@ module metrics
 go 1.19
 
 require (
-	github.com/jkstack/anet v0.0.0-20220825040413-818bc7f209d5
+	github.com/jaypipes/ghw v0.9.0
+	github.com/jkstack/anet v0.0.0-20220825062421-406a2f330850
 	github.com/jkstack/jkframe v1.0.7
 	github.com/jkstack/libagent v1.0.1
+	github.com/shirou/gopsutil v3.21.11+incompatible
 	github.com/shirou/gopsutil/v3 v3.22.7
 )
 
 require (
+	github.com/StackExchange/wmi v1.2.1 // indirect
 	github.com/btcsuite/winsvc v1.0.0 // indirect
 	github.com/cilium/ebpf v0.9.1 // indirect
 	github.com/containerd/cgroups v1.0.4 // indirect
 	github.com/coreos/go-systemd/v22 v22.3.2 // indirect
 	github.com/docker/go-units v0.4.0 // indirect
 	github.com/dustin/go-humanize v1.0.0 // indirect
+	github.com/ghodss/yaml v1.0.0 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/jaypipes/pcidb v1.0.0 // indirect
 	github.com/kardianos/service v1.2.1 // indirect
 	github.com/lufia/plan9stats v0.0.0-20220517141722-cf486979b281 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/opencontainers/runtime-spec v1.0.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/power-devops/perfstat v0.0.0-20220216144756-c35f1ee13d7c // indirect
-	github.com/shirou/gopsutil v3.21.11+incompatible // indirect
 	github.com/sirupsen/logrus v1.9.0 // indirect
 	github.com/tklauser/go-sysconf v0.3.10 // indirect
 	github.com/tklauser/numcpus v0.5.0 // indirect
 	github.com/yusufpapurcu/wmi v1.2.2 // indirect
 	golang.org/x/sys v0.0.0-20220804214406-8e32c043e418 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	howett.net/plist v1.0.0 // indirect
 )
--- internal/static.go
+++ internal/static.go
@@ -1,27 +1,38 @@
 package internal
 
 import (
+	"net"
+	"runtime"
+	"strconv"
+	"strings"
 	"time"
 
+	"github.com/jaypipes/ghw"
 	"github.com/jkstack/anet"
 	"github.com/jkstack/jkframe/logging"
+	"github.com/shirou/gopsutil/mem"
+	"github.com/shirou/gopsutil/v3/cpu"
+	"github.com/shirou/gopsutil/v3/disk"
 	"github.com/shirou/gopsutil/v3/host"
 )
 
 func getStatic() *anet.HMStaticPayload {
 	var ret anet.HMStaticPayload
 	ret.Time = time.Now()
-	info, err := host.Info()
-	if err != nil {
-		logging.Warning("get host.info: %v", err)
-	}
-	if info != nil {
-		fillStaticHostInfo(&ret, info)
-	}
+	fillStaticHostInfo(&ret)
+	fillStaticCpuInfo(&ret)
+	fillStaticMemoryInfo(&ret)
+	fillStaticDiskInfo(&ret)
+	fillStaticNetworkInfo(&ret)
 	return &ret
 }
 
-func fillStaticHostInfo(ret *anet.HMStaticPayload, info *host.InfoStat) {
+func fillStaticHostInfo(ret *anet.HMStaticPayload) {
+	info, err := host.Info()
+	if err != nil {
+		logging.Warning("get host.info: %v", err)
+		return
+	}
 	ret.Host.Name = info.Hostname
 	ret.Host.UpTime = time.Duration(info.Uptime) * time.Second
 	ret.OS.Name = info.OS
@@ -30,4 +41,117 @@ func fillStaticHostInfo(ret *anet.HMStaticPayload, info *host.InfoStat) {
 	ret.OS.Install = time.Unix(0, 0) // TODO
 	ret.Kernel.Version = info.KernelVersion
 	ret.Kernel.Arch = info.KernelArch
+}
+
+func fillStaticCpuInfo(ret *anet.HMStaticPayload) {
+	var err error
+	ret.CPU.Physical, err = cpu.Counts(false)
+	if err != nil {
+		logging.Warning("get physical cpu count: %v", err)
+	}
+	ret.CPU.Logical, err = cpu.Counts(true)
+	if err != nil {
+		logging.Warning("get logical cpu count: %v", err)
+	}
+	cores, err := cpu.Info()
+	if err != nil {
+		logging.Warning("get cpu.info: %v", err)
+		return
+	}
+	for _, core := range cores {
+		id, _ := strconv.ParseUint(core.CoreID, 10, 32)
+		physical, _ := strconv.ParseUint(core.PhysicalID, 10, 32)
+		ret.CPU.Cores = append(ret.CPU.Cores, anet.HMCore{
+			Processor: core.CPU,
+			Model:     core.ModelName,
+			Core:      int32(id),
+			Cores:     core.Cores,
+			Physical:  int32(physical),
+			Mhz:       core.Mhz,
+		})
+	}
+}
+
+func fillStaticMemoryInfo(ret *anet.HMStaticPayload) {
+	vm, err := mem.VirtualMemory()
+	if err != nil {
+		logging.Warning("get memory.info: %v", err)
+	}
+	if vm != nil {
+		ret.Memory.Physical = vm.Total
+	}
+	swap, err := mem.SwapMemory()
+	if err != nil {
+		logging.Warning("get swap.info: %v", err)
+	}
+	if swap != nil {
+		ret.Memory.Swap = swap.Total
+	}
+}
+
+func fillStaticDiskInfo(ret *anet.HMStaticPayload) {
+	block, err := ghw.Block()
+	if err != nil {
+		logging.Warning("get blocks: %v", err)
+	}
+	for _, disk := range block.Disks {
+		if disk.StorageController == ghw.STORAGE_CONTROLLER_UNKNOWN {
+			continue
+		}
+		var parts []string
+		for _, part := range disk.Partitions {
+			if runtime.GOOS == "linux" {
+				part.Name = "/dev/" + part.Name
+			}
+			parts = append(parts, part.Name)
+		}
+		ret.Disks = append(ret.Disks, anet.HMDisk{
+			Model:      disk.Model,
+			Total:      disk.SizeBytes,
+			Type:       disk.DriveType.String(),
+			Partitions: parts,
+		})
+	}
+	parts, err := disk.Partitions(false)
+	if err != nil {
+		logging.Warning("get partitions: %v", err)
+	}
+	for _, part := range parts {
+		usage, err := disk.Usage(part.Mountpoint)
+		if err != nil {
+			logging.Warning("get partition usage(%s): %v", part.Mountpoint, err)
+		}
+		info := anet.HMPartition{
+			Name:   part.Mountpoint,
+			FSType: part.Fstype,
+			Opts:   part.Opts,
+		}
+		if usage != nil {
+			info.Total = usage.Total
+		}
+		ret.Partitions = append(ret.Partitions, info)
+	}
+}
+
+func fillStaticNetworkInfo(ret *anet.HMStaticPayload) {
+	ret.GateWay = "TODO"
+	intfs, err := net.Interfaces()
+	if err != nil {
+		logging.Warning("get interfaces: %v", err)
+	}
+	for _, intf := range intfs {
+		addrs, _ := intf.Addrs()
+		ips := make([]string, len(addrs))
+		for i, addr := range addrs {
+			ips[i] = addr.String()
+		}
+		ret.Interface = append(ret.Interface, anet.HMInterface{
+			Index:   intf.Index,
+			Name:    intf.Name,
+			Mtu:     intf.MTU,
+			Flags:   strings.Split(intf.Flags.String(), "|"),
+			Mac:     intf.HardwareAddr.String(),
+			Address: ips,
+		})
+	}
 }
